f21bb6d5674429138b19c8fb8956804256cea534
TopLong <201497709@qq.com>
2021-04-22 15:53:35 +0800

    deploy

---
+++ precache-manifest.6361ad6b48a9cd260dcb17bda6a12426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0f1f06f48f1fa6794c",
+    "url": "/vue-memory-web/css/app.b91fe627.css"
+  },
+  {
+    "revision": "d66ceb01f69bf1f77b5ede4a2e6e4c47",
+    "url": "/vue-memory-web/index.html"
+  },
+  {
+    "revision": "5d0f1f06f48f1fa6794c",
+    "url": "/vue-memory-web/js/app.707e3f99.js"
+  },
+  {
+    "revision": "0ab3da36ad33823f822e",
+    "url": "/vue-memory-web/js/chunk-vendors.576205e2.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/vue-memory-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-memory-web/robots.txt"
+  },
+  {
+    "revision": "16804245427a581dba6fe22fe4eac629",
+    "url": "/vue-memory-web/web.png"
+  }
+]);
